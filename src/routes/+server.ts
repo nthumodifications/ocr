ca3a10b75bcc410210431de1c7d303bde5f741c5
Joshimello <joshualeanjw@gmail.com>
2023-12-25 22:09:43 +0800

    uwu

---
+++ src/routes/+server.ts
@@ -0,0 +1,28 @@
+import { json, error } from '@sveltejs/kit'
+import { createWorker } from 'tesseract.js'
+
+export const GET = async ({ url }) => {
+
+  let imgUrl = url.searchParams.get('url');
+
+  try {
+    const worker = await createWorker('eng');
+    await worker.setParameters({
+      tessedit_char_whitelist: '0123456789',
+    })
+
+    const result = await worker.recognize(imgUrl);
+    await worker.terminate();
+
+    return new json({
+      result: result.data.text
+    });
+  }
+
+  catch {
+    throw error(400, 'Sum ting wong')
+  }
+
+  throw error(400, 'Sum ting wong')
+
+}
